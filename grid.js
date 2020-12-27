1a7804ee6fe8f7e6a23775ec0b543a19313a10b8
Romit Khanal <16558256+upsteer@users.noreply.github.com>
2020-12-27 20:53:02 +0545

    trying day17

--- grid.js
+++ grid.js
@@ -7,6 +7,11 @@ class Grid{
 		this.xy[this.currentX][this.currentY] = 'X'
 		this.mapper = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 	}
+	set_different(scaffold, x, y){
+		this.xy = scaffold;
+		this.currentX = x;
+		this.currentY = y;
+	}
 	printing(){
 		this.xy.forEach(function(item){
 			console.log(item.join(''))
@@ -25,6 +30,24 @@ class Grid{
 		this.currentY += y_move;
 		this.xy[this.currentX][this.currentY] = shape;
 	}
+	get_possible_move(){
+		// Up/Down is X and Left/Right is Y
+		// First know where we can travel
+		var possible_moves = [];
+		this.mapper.forEach(function(directions, index){
+			var new_x = this.currentY+directions[0];
+			var new_y = this.currentX+directions[1];
+			if(new_y < 0 || new_x < 0){
+				return;
+			}
+			if(this.xy[new_x][new_y] == "#"){
+				possible_moves.push(index+1);
+			}
+		}, this)
+		return possible_moves;
+	}
+	move_until_out(){
+		var now = this.xy[this.currentY][this.currentX];
+	}
 }
-
 module.exports = Grid;
